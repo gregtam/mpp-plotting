8ff41204f3f51a345a8658e86ce9721ac35051b0
gregtam <gregtam1991@gmail.com>
2017-01-04 13:23:06 -0500

    Move plotting notebooks over from jupyter-notebook-template. That repository is designed to be copied for new project. The files in this new repository are here to illustrate how to use the files in the jupyter-notebook-template repository.

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# This credentials file includes important information for logging 
+# into a cluster such as GPDB or HAWQ. We use this to hide the information
+# from whatever file is logging into the DCA.
+
+login_info_dict = {'database': 'postgres', 
+                   'user': 'postgres',
+                   'password': '',
+                   'host': 'localhost'
+                  }
